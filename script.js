7ca1a6356f5a94737244f86ee7a2a45d5dcb1818
hamzayt577 <hamzayt577@gmail.com>
2021-03-25 15:18:53 +0400

    added income functionality

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
-const actualBudget = document.querySelector(".budget");
-const enterBudget = document.querySelector(".enter-budget");
-const submitBudgetBtn = document.querySelector(".submit-budget");
+const budget = prompt("What is your budget (please enter a number 😁)");
+const actualBudget = parseFloat(budget);
 
-const enterIncome = document.querySelector(".income-input");
-const descriptionIncome = document.querySelector(".income-description");
+const actualBudgetUI = document.querySelector(".budget");
+
+export const enterIncome = document.querySelector(".income-input");
+export const descriptionIncome = document.querySelector(".income-description");
 const submitIncomeBtn = document.querySelector(".submit-income");
 
-const enterExpense = document.querySelector(".expense-input");
-const descriptionExpense = document.querySelector(".expense-description");
+export const enterExpense = document.querySelector(".expense-input");
+export const descriptionExpense = document.querySelector(
+  ".expense-description"
+);
 const submitExpenseBtn = document.querySelector(".submit-expense");
 
 const expenseAndIncomeDiv = document.querySelector(".expense-and-income");
 
-const enterBudgetFunc = function () {
-  if (enterBudget.value === "") {
-    alert("Please enter a budget!");
+const incomeDescriptions = [];
+const incomeAmounts = [];
+
+document.querySelector(".budget").innerHTML = `Budget: ${actualBudget}`;
+
+const incomeFunc = function () {
+  if (enterIncome.value === "" && descriptionIncome.value === "") {
+    alert("Please enter income and income description");
   } else {
-    actualBudget.innerHTML = `Budget: ${enterBudget.value}`;
-    enterBudget.value = "";
+    let enterIncomeValue = enterIncome.value;
+    let descriptionIncomeValue = descriptionIncome.value;
+    incomeDescriptions.push(descriptionIncomeValue);
+    incomeAmounts.push(enterIncomeValue);
+    enterIncome.value = "";
+    descriptionIncome.value = "";
+    loopIncomeDesc();
+    loopIncomeAmount();
+    renderIncomeHTML(descriptionIncomeValue, enterIncomeValue);
   }
 };
 
-submitBudgetBtn.addEventListener("click", enterBudgetFunc);
+const loopIncomeDesc = function () {
+  incomeDescriptions.forEach((desc) => {});
+};
+
+const loopIncomeAmount = function () {
+  incomeAmounts.forEach((amount) => {
+    let amountNumber = parseInt(amount);
+    actualBudgetUI.innerHTML = `Budget: ${amountNumber + actualBudget}`;
+  });
+};
+
+const renderIncomeHTML = function (name, value) {
+  const markup = `
+  <div class="income">
+      <span class="income-name">${name}</span>
+      <span class="income-amount">${value}</span>
+    </div>
+  `;
+
+  expenseAndIncomeDiv.insertAdjacentHTML("afterbegin", markup);
+};
+
+submitIncomeBtn.addEventListener("click", incomeFunc);
